c91ac42b1674e7b507c24df368ae57691b1640cb
Junior <junior.silva_costa@live.com>
2024-02-18 17:14:36 -0300

    feat: finaliza conversão de csv e envio ao sqs

--- src/app.js
+++ src/app.js
@@ -7,6 +7,26 @@ const logger = new Logger({ serviceName: 'integracao-s3-lambda-sqs' });
 const s3Client = new S3Client();
 const sqsClient = new SQSClient();
 
+async function converteCSV(Body) {
+    return new Promise((resolve, reject) => {
+        let dataCsv = [];
+        Body
+            .pipe(parse({ headers: true }))
+            .on('error', error => {
+                reject(logger.error('Erro ao realizar conversão CSV', error));
+            })
+            .on('data', linha => {
+                logger.info('Linha convertida', linha);
+                dataCsv.push(linha);
+            })
+            .on('end', (countLinha) => {
+                logger.info('Conversão CSV finalizada. ' + countLinha + ' linhas afetadas.');
+                logger.info('DataCSV', dataCsv);
+                resolve(dataCsv);
+            });
+    });
+}
+
 export const app = async (event, context) => {
     logger.info('Iniciando lambda!');
     logger.info('Event recebido', event);
@@ -20,40 +40,23 @@ export const app = async (event, context) => {
         // Obtendo objeto do S3
         const { Body } = await s3Client.send(new GetObjectCommand({ Bucket: bucketName, Key: keyObject }));
 
-        // Convertendo CSV
-        const data = Body
-            .pipe(parse({ headers: false }))
-            .on('error', error => console.log('ERRO NO PARSE: ', error))
-            .on('data', row => console.log('ROW NO PARSE: ', row))
-            .on('end', rowCount => {
-                console.log(`Parsed ${rowCount} rows`);
-            });
+        logger.info('Iniciando conversão do CSV');
 
-        logger.info('Sucesso ao realizar conexão com o S3');
-    } catch (error) {
-        logger.error('Erro ao realizar conexão com S3', error);
-        throw new Error('Erro ao realizar conexão com S3')
-    }
+        let csvConvertido = await converteCSV(Body);
 
-    try {
         // Enviando para a fila SQS
-        const responseSQS = await sqsClient.send(new SendMessageCommand(
+        await sqsClient.send(new SendMessageCommand(
             {
-                QueueUrl: 'https://sqs.sa-east-1.amazonaws.com/279608136202/integracao-fila.fifo',
-                MessageBody: '',
-                MessageGroupId: 'messageGroupTeste',
-                MessageDeduplicationId: 'messageDeduplicationIdTeste'
+                QueueUrl: process.env.URL_SQS,
+                MessageBody: JSON.stringify(csvConvertido),
+                MessageGroupId: keyObject,
+                MessageDeduplicationId: keyObject
             }
         ));
 
-        logger.info('Sucesso ao enviar mensagem ao SQS');
+        logger.info('Mensagem enviado ao SQS com sucesso!');
     } catch (error) {
-        logger.info('Erro ao enviar mensagem ao SQS', error);
-        throw new Error('Erro ao enviar mensagem ao SQS');
-    }
-
-    return {
-        statusCode: 200,
-        body: 'Lambda executada com sucesso!'
+        logger.error('Ocorreu um erro', error);
+        throw new Error('Ocorreu um erro')
     }
 }
